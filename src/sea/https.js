f299526d3ce8cf752c67011af3c66686171f7bc9
linhhm@outlook.com <linhhm@outlook.com>
2024-04-15 01:06:55 +0700

    remove console.log

--- src/sea/https.js
+++ src/sea/https.js
@@ -1,12 +1,13 @@
 
-    var SEA = require('./root');
-    try{ if(SEA.window){
-      if(location.protocol.indexOf('s') < 0
+var SEA = require('./root');
+try {
+  if (SEA.window) {
+    if (location.protocol.indexOf('s') < 0
       && location.host.indexOf('localhost') < 0
       && ! /^127\.\d+\.\d+\.\d+$/.test(location.hostname)
-      && location.protocol.indexOf('file:') < 0){
-        console.warn('HTTPS needed for WebCrypto in SEA, redirecting...');
-        location.protocol = 'https:'; // WebCrypto does NOT work without HTTPS!
-      }
-    } }catch(e){}
-  
+      && location.protocol.indexOf('file:') < 0) {
+      /*/*console.warn('HTTPS needed for WebCrypto in SEA, redirecting...');*/
+      location.protocol = 'https:'; // WebCrypto does NOT work without HTTPS!
+    }
+  }
+} catch (e) { }
